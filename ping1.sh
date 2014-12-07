120d6bf3c89e914fb7505c42ac538ef8bdad022d
root <root@localhost.localdomain>
2014-12-07 21:41:41 +0800

    分别用while和for对ping 172.16.250.0.16,在线用绿色表示，不在线用红色表示，中间可以中断

---
+++ ping1.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#****************************************************
+#         Author: dudu   471906904@qq.com
+#  Last modified: 2014-12-05 12:33
+#       Filename: ping.sh
+#****************************************************
+function ping_fun()
+{
+	for((i=250;i<=255;i++))
+	do
+		for j in {0..255}
+		do
+			ping -c1 -w1 172.16.$i.$j &> /dev/null
+			if [ $? -eq 0 ]
+			then
+				echo -e "\033[32m 172.16.$i.$j is online \033[0m"  
+				let online++
+			else
+				echo -e "\033[31m 172.16.$i.$j is offline\033[0m"
+				let offlint++
+			fi
+		done
+	done
+}
+
+function fun_while()
+{
+	declare -i iwhile=250
+	declare -i iwhile=0
+	while [ $iwhile -lt 255 ]
+	do
+		while [ $jwhile -lt 255 ]
+		do
+			ping -c1 -w1 172.16.$iwhile.$jwhile &> /dev/null
+			if [ $? -eq 0 ]
+                        then
+                                echo -e "\033[32m 172.16.$iwhile.$jwhile is online \033[0m"  
+                                let while_onlie++
+                        else
+                                echo -e "\033[31m 172.16.$while.$jwhile is offline\033[0m"
+                                let while_offline++
+                        fi
+			let $jwhile++
+		done
+		let $iwhile++
+	done
+}
+trap 'echo "quit"; exit 1' SIGINT
+ping_fun
+echo "在线的是$online"
+echo "没在线的$offline"
+echo -e "\033[34m 172.16.$i.$j is offline\033[0m"
